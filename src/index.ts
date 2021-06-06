3e223f8490e0457fbd01106eacbf6d5df384f9ad
DESKTOP-BIF672G\wdjty <wdjty326@gmail.com>
2021-06-06 18:11:47 +0900

    return promise

--- src/index.ts
+++ src/index.ts
@@ -40,100 +40,132 @@ class NoDB {
 
 	// 신규 데이터 파일을 생성합니다.
 	public createFile(fileName: string, ...args: (HeaderOption | string)[]) {
-		// 파일 형식은 json 형식으로 가져갑니다.
-		fileName = !/\.(json)$/.test(fileName) ? fileName + ".json" : fileName;
-		const filePath = path.resolve(JSON_FILE_PATH, fileName);
+		return new Promise<void>((resolve, reject) => {
+			try {
+				// 파일 형식은 json 형식으로 가져갑니다.
+				fileName = !/\.(json)$/.test(fileName) ? fileName + ".json" : fileName;
+				const filePath = path.resolve(JSON_FILE_PATH, fileName);
 
-		if (fs.existsSync(filePath)) throw Error("File already exists::" + fileName);
+				if (fs.existsSync(filePath)) throw Error("File already exists::" + fileName);
 
-		const header: FileHeader = {};
-		args.forEach((v) => {
-			if (typeof v === "string") header[v] = {};
-			else header[v.name] = { primary: v.primary, };
+				const header: FileHeader = {};
+				args.forEach((v) => {
+					if (typeof v === "string") header[v] = {};
+					else header[v.name] = { primary: v.primary, };
+				});
+
+				fs.writeFileSync(filePath, JSON.stringify({ header, data: [] }), { encoding: "utf-8" });
+				resolve();
+			} catch (e) {
+				reject(e);
+			}
 		});
-
-		fs.writeFileSync(filePath, JSON.stringify({ header, data: [] }), { encoding: "utf-8" });
 	}
 
 	// 데이터 파일 헤더를 변경합니다.
 	public updateFile(fileName: string, ...args: (HeaderOption | string)[]) {
-		this.readFile(fileName)
-			.then(({ filePath, data, primary }) => {
-				const header: FileHeader = {};
-				args.forEach((v) => {
-					if (typeof v === "string") header[v] = { };
-					else header[v.name] = { primary: v.primary, };
-				});
+		return new Promise<void>((resolve, reject) => {
+			this.readFile(fileName)
+				.then(({ filePath, data, primary }) => {
+					const header: FileHeader = {};
+					args.forEach((v) => {
+						if (typeof v === "string") header[v] = {};
+						else header[v.name] = { primary: v.primary, };
+					});
 
-				fs.writeFileSync(filePath, JSON.stringify({
-					...data,
-					...{
-						header: {
-							...data.header,
-							...header,
+					fs.writeFileSync(filePath, JSON.stringify({
+						...data,
+						...{
+							header: {
+								...data.header,
+								...header,
+							},
 						},
-					},
-				}), { encoding: "utf-8" });
-			});
+					}), { encoding: "utf-8" });
+					resolve();
+				})
+				.catch((e) => {
+					reject(e);
+				});
+		});
 	}
 
 	// 데이터 파일을 제거합니다.
 	public deleteFile(fileName: string) {
-		// 파일 형식은 json 형식으로 가져갑니다.
-		fileName = !/\.(json)$/.test(fileName) ? fileName + ".json" : fileName;
-		const filePath = path.resolve(JSON_FILE_PATH, fileName);
-		if (fs.existsSync(filePath)) fs.rmSync(filePath);
+		return new Promise<void>((resolve, reject) => {
+			try {
+				// 파일 형식은 json 형식으로 가져갑니다.
+				fileName = !/\.(json)$/.test(fileName) ? fileName + ".json" : fileName;
+				const filePath = path.resolve(JSON_FILE_PATH, fileName);
+				if (fs.existsSync(filePath)) fs.rmSync(filePath);
+				resolve();
+			} catch (e) {
+				reject(e);
+			}
+		});
 	}
 
 	// 신규 데이터를 추가합니다.
 	public insertData<T extends { [key: string]: any }>(fileName: string, ...args: T[]) {
-		this.readFile<T>(fileName)
-			.then(({ filePath, data, primary }) => {
-				if (data.data.some((v) => {
-					const idx = args.findIndex((x) => primary.some((y) => ((x as any)[y] === (v as any)[y])));
-					return idx > -1;
-				})) throw Error("Already Primary key");
+		return new Promise<void>((resolve, reject) => {
+			this.readFile<T>(fileName)
+				.then(({ filePath, data, primary }) => {
+					if (data.data.some((v) => {
+						const idx = args.findIndex((x) => primary.some((y) => ((x as any)[y] === (v as any)[y])));
+						return idx > -1;
+					})) throw Error("Already Primary key");
 
-				data.data.push(...args);
-				fs.writeFileSync(filePath, JSON.stringify(data), { encoding: "utf-8" });
-			})
-			.catch((e) => {
-				console.error(e);
-			});
+					data.data.push(...args);
+					fs.writeFileSync(filePath, JSON.stringify(data), { encoding: "utf-8" });
+					resolve();
+				})
+				.catch((e) => {
+					console.error(e);
+					reject(e);
+				});
+		});
 	}
 
 	// 데이터를 업데이트합니다. primary 가 없으면 업데이트를 하지 않습니다.
 	public updateData<T extends { [key: string]: any }>(fileName: string, ...args: T[]) {
-		this.readFile<T>(fileName)
-			.then(({ filePath, data, primary }) => {
-				data.data = data.data.map((v) => {
-					const idx = args.findIndex((x) => primary.some((y) => (x[y] === v[y])));
-					if (idx > -1)
-						Object.keys(args[idx]).forEach((k) => ((v as any)[k] = args[idx][k]));
-					return v;
-				});
+		return new Promise<void>((resolve, reject) => {
+			this.readFile<T>(fileName)
+				.then(({ filePath, data, primary }) => {
+					data.data = data.data.map((v) => {
+						const idx = args.findIndex((x) => primary.some((y) => (x[y] === v[y])));
+						if (idx > -1)
+							Object.keys(args[idx]).forEach((k) => ((v as any)[k] = args[idx][k]));
+						return v;
+					});
 
-				fs.writeFileSync(filePath, JSON.stringify(data), { encoding: "utf-8" });
-			})
-			.catch((e) => {
-				console.error(e);
-			});
+					fs.writeFileSync(filePath, JSON.stringify(data), { encoding: "utf-8" });
+					resolve();
+				})
+				.catch((e) => {
+					console.error(e);
+					reject(e);
+				});
+		});
 	}
 
 	// 데이터를 제거합니다. primary 가 없으면 제거하지 않습니다.
 	public deleteData<T extends { [key: string]: any }>(fileName: string, ...args: T[]) {
-		this.readFile<T>(fileName)
-			.then(({ filePath, data, primary }) => {
-				data.data = data.data.filter((v) => {
-					const idx = args.findIndex((x) => primary.some((y) => (x[y] === v[y])));
-					return idx === -1;
-				});
+		return new Promise<void>((resolve, reject) => {
+			this.readFile<T>(fileName)
+				.then(({ filePath, data, primary }) => {
+					data.data = data.data.filter((v) => {
+						const idx = args.findIndex((x) => primary.some((y) => (x[y] === v[y])));
+						return idx === -1;
+					});
 
-				fs.writeFileSync(filePath, JSON.stringify(data), { encoding: "utf-8" });
-			})
-			.catch((e) => {
-				console.error(e);
-			});
+					fs.writeFileSync(filePath, JSON.stringify(data), { encoding: "utf-8" });
+					resolve();
+				})
+				.catch((e) => {
+					console.error(e);
+					reject(e);
+				});
+		});
 	}
 
 	public selectOne<T extends { [key: string]: any }>(fileName: string, fn: (data: T) => boolean) {
